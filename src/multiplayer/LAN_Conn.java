4206955f679d8a765858285fe26dd28e3decf9e4
Ahmet Yanık <73365349+Ahmetyanikk@users.noreply.github.com>
2022-04-30 22:57:15 +0300

    Update LAN_Conn.java

--- src/multiplayer/LAN_Conn.java
+++ src/multiplayer/LAN_Conn.java
@@ -5,56 +5,32 @@ import game.Model;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
-
-/**
- * Interface which represents the LAN connection and which is
- * to be implemented e.g. by a server/client 
- * 
- * 
- */
+//bağlantıyı illüstre eden bir arayüz.
 public abstract class LAN_Conn extends Thread {
     Model model;
     boolean isConnected = false;
     Socket socket;
-    
-    /**
-     * @param b the message to be sent 
-     * @throws IOException
-     */
+// giden mesajı gösteren b
     public void send(int b) throws IOException{
         System.out.println("LAN_Conn: Going to send draw...");
         socket.getOutputStream().write(b);
         System.out.println("LAN_Conn: Sent draw");
     }
-  
-    /**
-     * @return the received message or -1 in case of an error
-     * @throws IOException 
-     * @throws InterruptedException 
-     */
+// ya mesaj döner yada hata anlamında -1
     public int receive() throws IOException, InterruptedException{
         InputStream stream = socket.getInputStream();
         
-        //Is data available?
+        //veriye ulaşabildi mi?
         while (stream.available() == 0){
         	Thread.sleep(100);
         }
-    	//Read and return incoming data
+    	//gelen veriyi okuyup döndürür
     	return stream.read();
     }
 
-    /**
-     * Establishes the LAN connection
-     * 
-     * @throws IOException
-     */
+// bağlantı kurulur
     public abstract void init(String server_addr) throws IOException;
-
-    /**
-     * Closes the {@code socket}
-     * 
-     * @throws IOException
-     */
+// bağlantıyı kapatır 
     public void terminate() throws IOException {
         if (socket != null)
             socket.close();
